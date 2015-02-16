d95a5ed77864c8cacdbf7717ec4f843d89bfc3e4
andrepolischuk <andre.polischuk@gmail.com>
2015-02-16 15:34:26 +0300

    Add IE8- support

--- index.js
+++ index.js
@@ -38,69 +38,111 @@ var types =  [
 ];
 
 /**
- * Test element exist
- * @param  {String} type
- * @return {Object}
- * @api private
+ * Messages flow
  */
 
-function existElement(type) {
+var flow = {};
 
-  var element = document.querySelector([
-    '.',
+/**
+ * Message
+ * @param {String} type
+ * @param {String} text
+ * @param {Number} delay
+ * @api public
+ */
+
+function Message(type, text, delay) {
+
+  this.id = [
     options.element,
-    '-',
-    type
-  ].join(''));
+    '_',
+    (new Date()).valueOf(),
+    (Math.random() * 1000000000).toFixed()
+  ].join('');
 
-  if (!element) {
-
-    element = document.createElement('div');
-
-    element.style.display = 'none';
-    element.style.opacity = '0.0';
-
-    element.style.transition = [
-      'opacity',
-      options.speed / 1000 + 's',
-      'ease-in-out'
-    ].join(' ');
-
-    element.className = [
-      options.element,
-      ' ',
-      options.element,
-      '-',
-      type
-    ].join('');
-
-    element.setAttribute('role', type);
-
-    document.getElementsByTagName('body')[0].appendChild(element);
-
-    element.onclick = function() {
-      hide(this);
-    };
-
-  }
-
-  return element;
+  this.type = type;
+  this.text = text.replace(/(<script.*>.*<\/script>)/gim, '');
+  this.delay = delay + options.speed || null;
+  this.element = appendElement(this);
 
 }
 
 /**
+ * Show message
+ * @api public
+ */
+
+Message.prototype.show = function() {
+
+  var self = this;
+  self.element.style.display = 'block';
+
+  setTimeout(function() {
+    self.element.style.opacity = '1.0';
+  }, 50);
+
+  if (self.delay) {
+    setTimeout(self.hide, self.delay);
+  }
+
+};
+
+/**
  * Hide message
- * @param {Object} element
+ * @api public
+ */
+
+Message.prototype.hide = function() {
+
+  var self = this;
+  self.element.style.opacity = '0.0';
+
+  setTimeout(function() {
+    self.element.style.display = 'none';
+    delete flow[self.id];
+  }, options.speed);
+
+};
+
+/**
+ * Append element to DOM
+ * @param  {Object} message
+ * @return {Object}
  * @api private
  */
 
-function hide(element) {
+function appendElement(message) {
 
+  var element = document.createElement('div');
+
+  element.style.display = 'none';
   element.style.opacity = '0.0';
 
-  setTimeout(function() {
-    element.style.display = 'none';
-  }, options.speed);
+  element.style.transition = [
+    'opacity',
+    options.speed / 1000 + 's',
+    'ease-in-out'
+  ].join(' ');
+
+  element.className = [
+    options.element,
+    ' ',
+    options.element,
+    '-',
+    message.type
+  ].join('');
+
+  element.id = message.id;
+  element.innerHTML = message.text;
+  element.setAttribute('role', message.type);
+
+  document.getElementsByTagName('body')[0].appendChild(element);
+
+  element.onclick = function() {
+    message.hide();
+  };
+
+  return element;
 
 }
 
@@ -119,30 +161,15 @@ function show(type) {
       return;
     }
 
-    var elements = document.querySelectorAll('.' + options.element);
-
-    for (var el = 0; el < elements.length; el++) {
-      if (elements[el].getAttribute('role') !== type) {
-        hide(elements[el]);
+    for (var fl in flow) {
+      if (flow.hasOwnProperty(fl)) {
+        flow[fl].hide();
       }
     }
 
-    var element = existElement(type);
-
-    element.innerHTML = text.replace(/(<script.*>.*<\/script>)/gim, '');
-    element.style.display = 'block';
-
-    setTimeout(function() {
-      element.style.opacity = '1.0';
-    }, 50);
-
-    if (delay) {
-
-      setTimeout(function() {
-        hide(element);
-      }, delay + options.speed);
-
-    }
+    var message = new Message(type, text, delay);
+    flow[message.id] = message;
+    message.show();
 
   };
 
@@ -164,6 +191,7 @@ function messg(text, type, delay) {
   }
 
   show(type)(text, delay);
+
 }
 
 /**
